09881b2237effc07c09cd78e4ce8cb7343de9d95
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2020-04-14 15:21:18 +0200

    Added optional word length

--- hangman.py
+++ hangman.py
@@ -24,6 +24,18 @@ def choose_rand(elements):
     return elements[random.randint(0, len(elements) - 1)]
 
 
+def get_int(prompt, separator=": "):
+    """
+    Prompts the user with the given prompt, and returns the int the user enters. Returns false if the user doesn't enter an int.
+    :param prompt: The prompt to give the user for the int.
+    :return: The int the user entered, or False if the user didn't enter an int.
+    """
+    try:
+        return int(input(prompt + separator))
+    except Exception as e:
+        return False
+
+
 def will_play():
     """
     If the user wants to play a game of hangman.
@@ -43,38 +55,50 @@ def will_play():
 
 
 def find_nth(haystack, needle, n):
-    parts= haystack.split(needle, n+1)
-    if len(parts)<=n+1:
+    parts = haystack.split(needle, n+1)
+    if len(parts) <= n+1:
         return -1
     return len(haystack)-len(parts[-1])-len(needle)
 
 
 if __name__ == '__main__':
-    word_list = read_json_file(r"vendor\dwyl\english-words\words_dictionary.json")
+    word_list = read_json_file(
+        r"vendor\dwyl\english-words\words_dictionary.json")
     # word_list = read_json_file(r"vendor\engineer-man\names\name-list.json")
 
     while will_play():
         """
         Container game loop. While the user wants to play the game, this loop loops.
         """
-        word = choose_rand(word_list)
-        word = word.upper()
         guessed_letters = []
         correct_letters = []
         attempts = 0
-        try:
-            max_attempts = int(input("How many guesses do you want? Please choose an integer: "))
-        except Exception as e:
-            print("You cannot choose this number because it is not an integer. Defaulted to 10.")
-            max_attempts = 10
+
+        get_result = get_int(
+            "How many guesses do you want? Please choose an integer")
+        max_attempts = get_result if get_result != False else 10
+
+        get_result = get_int(
+            "How long can the word be? Please choose a max integer above 0")
+        max_length = get_result if get_result != False else 1000
+        while max_length <= 0:
+            get_result = get_int(
+                "How long can the word be? Please choose a max integer above 0")
+            max_length = get_result if get_result != False else 1000
+
         last_guessed = ""
+
+        word = choose_rand(word_list).upper()
+        while len(word) > max_length:
+            word = choose_rand(word_list).upper()
 
         print(f"The word is a {len(word)} letter word.")
         while True:
             """
             Main game loop. Game logic for each round is handled here.
             """
-            last_guessed = input("\nGuess a letter or the word you think it is: ").upper()
+            last_guessed = input(
+                "\nGuess a letter or the word you think it is: ").upper()
             if not last_guessed:
                 print("You need to guess something.")
                 continue
@@ -82,7 +106,8 @@ if __name__ == '__main__':
             attempts += 1
 
             if last_guessed == word:
-                print(f"Correct! The word is {word}! You took {attempts} attempts to guess it!")
+                print(
+                    f"Correct! The word is {word}! You took {attempts} attempts to guess it!")
                 break
 
             if len(last_guessed) <= 1:
@@ -95,7 +120,10 @@ if __name__ == '__main__':
                 if word.__contains__(last_guessed[0]):
                     print(f"The letter {last_guessed[0]} was in the word!")
                     for i in range(word.count(last_guessed[0])):
-                        correct_letters.insert(find_nth(word, last_guessed[0], i), last_guessed[0])
+                        print(i, find_nth(word, last_guessed[0], i))
+                        correct_letters.insert(
+                            find_nth(word, last_guessed[0], i) - 1,
+                            last_guessed[0])
                 else:
                     print(f"The letter {last_guessed[0]} is not in the word.")
 
@@ -106,5 +134,6 @@ if __name__ == '__main__':
                 print(f"Correct guesses so far: {', '.join(correct_letters)}")
 
             if attempts > max_attempts:
-                print(f"You used up all of your tries. The word was {word}. Better luck next time!")
+                print(
+                    f"You used up all of your tries. The word was {word}. Better luck next time!")
                 break
